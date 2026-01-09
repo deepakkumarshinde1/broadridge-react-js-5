92a65587059eb418720e9ab20e891a77bc7d8f95
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2026-01-09 11:39:01 +0530

    Login and Registration

--- DAY-5/blog-app/src/components/Login.jsx
+++ DAY-5/blog-app/src/components/Login.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  let [loginUser, setLoginUser] = useState({
+    username: "",
+    password: "",
+  });
+  let inputChange = (event) => {
+    let { name, value } = event.target;
+    setLoginUser({ ...loginUser, [name]: value });
+  };
+  let makeLogin = (event) => {
+    event.preventDefault();
+
+    let usersList = localStorage.getItem("users");
+    usersList = usersList ? JSON.parse(usersList) : [];
+
+    if (usersList.length === 0) {
+      alert("user not found, kindly create an account");
+      return false;
+    }
+
+    let isUserExists = usersList.find(
+      (user) =>
+        loginUser.username === user.emailId &&
+        loginUser.password === user.password
+    );
+    if (isUserExists) {
+      alert("Login successfully");
+      return false;
+    } else {
+      alert("Wrong username or password.");
+    }
+  };
+
   return (
     <div className="login-page">
       <div className="login-card">
@@ -14,13 +46,19 @@ const Login = () => {
           Welcome back! Please login to your account
         </p>
 
-        <form className="login-form">
+        <form className="login-form" onSubmit={makeLogin}>
           {/* Email */}
           <div className="form-group">
             <label>
               <i className="fa fa-envelope"></i> Email Address
             </label>
-            <input type="email" placeholder="you@example.com" />
+            <input
+              value={loginUser.username}
+              name="username"
+              onChange={inputChange}
+              type="email"
+              placeholder="you@example.com"
+            />
           </div>
 
           {/* Password */}
@@ -28,18 +66,13 @@ const Login = () => {
             <label>
               <i className="fa fa-key"></i> Password
             </label>
-            <input type="password" placeholder="Enter your password" />
-          </div>
-
-          {/* Options */}
-          <div className="login-options">
-            <label className="remember">
-              <input type="checkbox" /> Remember me
-            </label>
-
-            <a href="#" className="forgot">
-              Forgot password?
-            </a>
+            <input
+              value={loginUser.password}
+              name="password"
+              onChange={inputChange}
+              type="password"
+              placeholder="Enter your password"
+            />
           </div>
 
           {/* Button */}
--- DAY-5/blog-app/src/components/Register.jsx
+++ DAY-5/blog-app/src/components/Register.jsx
@@ -1,8 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Register.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 const Register = () => {
+  let navigate = useNavigate();
+  let [newUser, setNewUser] = useState({
+    fullName: "",
+    emailId: "",
+    password: "",
+    confirmPassword: "",
+    iAgree: false,
+  });
+
+  let inputChange = (event) => {
+    let { name, value } = event.target;
+    if (name === "iAgree") {
+      value = !newUser.iAgree;
+    }
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  let lengthValidation = (value, message) => {
+    if (value.trim().length === 0) {
+      alert(message);
+      return true;
+    }
+    return false;
+  };
+  let saveUserForm = (event) => {
+    event.preventDefault();
+
+    if (lengthValidation(newUser.fullName, "Enter Full Name")) {
+      return false;
+    }
+    if (lengthValidation(newUser.emailId, "Enter Email Id")) {
+      return false;
+    }
+    if (lengthValidation(newUser.password, "Enter Password")) {
+      return false;
+    }
+    if (lengthValidation(newUser.confirmPassword, "Enter Confirm Password")) {
+      return false;
+    }
+
+    if (newUser.password !== newUser.confirmPassword) {
+      alert("Password and Confirm Password dose not match");
+      return false;
+    }
+
+    if (newUser.iAgree === false) {
+      alert("Kindly accept the teams and condition");
+      return false;
+    }
+
+    let usersList = localStorage.getItem("users");
+    usersList = usersList ? JSON.parse(usersList) : [];
+
+    let isUserExists = usersList.find(
+      (user) => newUser.emailId === user.emailId
+    );
+    if (isUserExists) {
+      alert("Email already registered, kindly user other");
+      return false;
+    }
+    // convert array/object/jsonArray to string => JSON.stringify(value);
+    // convert a string to array/object/jsonArray = JSON.parse(value)
+    usersList.push({ ...newUser, id: uuidv4() });
+
+    localStorage.setItem("users", JSON.stringify(usersList));
+    alert("Registration completed successfully, please login in.");
+    navigate("/login");
+  };
+
   return (
     <div className="register-page">
       <div className="register-card">
@@ -14,13 +84,19 @@ const Register = () => {
           Join us and start sharing your ideas
         </p>
 
-        <form className="register-form">
+        <form className="register-form" onSubmit={saveUserForm}>
           {/* Name */}
           <div className="form-group">
             <label>
               <i className="fa fa-user"></i> Full Name
             </label>
-            <input type="text" placeholder="Enter your full name" />
+            <input
+              type="text"
+              value={newUser.fullName}
+              onChange={inputChange}
+              name="fullName"
+              placeholder="Enter your full name"
+            />
           </div>
 
           {/* Email */}
@@ -28,7 +104,13 @@ const Register = () => {
             <label>
               <i className="fa fa-envelope"></i> Email Address
             </label>
-            <input type="email" placeholder="you@example.com" />
+            <input
+              type="email"
+              placeholder="you@example.com"
+              value={newUser.emailId}
+              onChange={inputChange}
+              name="emailId"
+            />
           </div>
 
           {/* Password */}
@@ -36,7 +118,13 @@ const Register = () => {
             <label>
               <i className="fa fa-key"></i> Password
             </label>
-            <input type="password" placeholder="Create a password" />
+            <input
+              type="password"
+              placeholder="Create a password"
+              value={newUser.password}
+              onChange={inputChange}
+              name="password"
+            />
           </div>
 
           {/* Confirm Password */}
@@ -44,14 +132,25 @@ const Register = () => {
             <label>
               <i className="fa fa-lock"></i> Confirm Password
             </label>
-            <input type="password" placeholder="Confirm your password" />
+            <input
+              type="password"
+              placeholder="Confirm your password"
+              value={newUser.confirmPassword}
+              onChange={inputChange}
+              name="confirmPassword"
+            />
           </div>
 
           {/* Terms */}
           <div className="terms">
             <label>
-              <input type="checkbox" />I agree to the{" "}
-              <a href="#">Terms & Conditions</a>
+              <input
+                type="checkbox"
+                checked={newUser.iAgree}
+                name="iAgree"
+                onChange={inputChange}
+              />
+              I agree to the <a href="#">Terms & Conditions</a>
             </label>
           </div>
 
